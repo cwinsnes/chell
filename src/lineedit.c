d7dcd0781ccd5c036f54e49e2d185ce01f709eb1
Casper Winsnes <cwinsnes92@gmail.com>
2019-07-09 18:07:15 +0200

    Add single character stepping forward and backward

--- src/lineedit.c
+++ src/lineedit.c
@@ -5,7 +5,15 @@
 #include <unistd.h>
 #include <string.h>
 
+#define ESC_VAL '\033'
+#define ESC_UP 'A'
+#define ESC_DOWN 'B'
+#define ESC_RIGHT 'C'
+#define ESC_LEFT 'D'
+#define CTRL_B 0x2
+#define CTRL_F 0x6
 #define DEFAULT_BUF_SIZE 32
+#define KEY_ESC 27
 #define KEY_BCKSPC 8
 #define KEY_DELETE 127
 #define KEY_NEWLINE '\n'
@@ -31,6 +39,39 @@ void _ch_readline_setup(void)
 }
 
 
+void _step_backward(size_t *bufindex)
+{
+  if (*bufindex > 0) {
+    *bufindex -= 1;
+  }
+}
+
+void _step_forward(size_t *bufindex, size_t *buflen)
+{
+  if (*bufindex < *buflen) {
+    *bufindex += 1;
+  }
+}
+
+void _handle_esc(char **buffer, size_t *bufindex, size_t *buflen)
+{
+  getc(stdin); // Skip '['
+  char c = getc(stdin);
+  switch(c) {
+  case ESC_UP:
+    break;
+  case ESC_DOWN:
+    break;
+  case ESC_LEFT:
+    _step_backward(bufindex);
+    break;
+  case ESC_RIGHT:
+    _step_forward(bufindex, buflen);
+    break;
+  }
+}
+
+
 char* ch_readline(const char *prompt, size_t promptlen)
 {
   _ch_readline_setup();
@@ -38,6 +79,7 @@ char* ch_readline(const char *prompt, size_t promptlen)
   char* buffer = malloc(sizeof(char) * DEFAULT_BUF_SIZE);
   buffer[DEFAULT_BUF_SIZE - 1] = '\0';
   size_t bufindex = 0;
+  size_t buflen = 0;
   size_t bufsize = DEFAULT_BUF_SIZE;
   char c;
   if (!prompt) {
@@ -51,6 +93,15 @@ char* ch_readline(const char *prompt, size_t promptlen)
     switch (c) {
     case KEY_NEWLINE:
       break;
+    case ESC_VAL:
+      _handle_esc(&buffer, &bufindex, &buflen);
+      break;
+    case CTRL_B:
+      _step_backward(&bufindex);
+      break;
+    case CTRL_F:
+      _step_forward(&bufindex, &buflen);
+      break;
     case KEY_DELETE: //Do same as backspace for now
     case KEY_BCKSPC:
       if (bufindex == 0) {
@@ -60,13 +111,23 @@ char* ch_readline(const char *prompt, size_t promptlen)
 	buffer[i-1] = buffer[i];
       }
       bufindex -= 1;
+      buflen -= 1;
       break;
     default:
+      for (i=buflen+1; i>bufindex; --i) {
+	buffer[i] = buffer[i-1];
+      }
       buffer[bufindex] = c;
       bufindex += 1;
+      buflen += 1;
+
+      if (buflen >= bufsize) {
+	bufsize *= 2;
+	buffer = realloc(buffer, bufsize);
+      }
     }
 
-    buffer[bufindex] = '\0';
+    buffer[buflen] = '\0';
     clearline();
     printf("%s%s", prompt, buffer);
     cursorpos(bufindex+promptlen);
