1d7c807de7f39b2bd0684320bd316fc120072229
cwinsnes <cwinsnes92@gmail.com>
2019-11-13 13:29:18 +0100

    Keymovement (#3)
    * Add basic parsing for pipes
    
    * make basic piping work
    
    * Make piping wait for all child processes to finish
    
    * Add single character stepping forward and backward
    
    * Add step word forward/backward
    
    * Fix gitignore to ignore tags files

--- src/lineedit.c
+++ src/lineedit.c
@@ -4,8 +4,19 @@
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
+#include <limits.h>
 
+#define ESC_VAL '\033'
+#define ESC_NUL 0x00
+#define ESC_UP 'A'
+#define ESC_DOWN 'B'
+#define ESC_RIGHT 'C'
+#define ESC_LEFT 'D'
+#define CTRL_B 0x2
+#define CTRL_F 0x6
 #define DEFAULT_BUF_SIZE 32
+#define KEY_ESC 27
 #define KEY_BCKSPC 8
 #define KEY_DELETE 127
 #define KEY_NEWLINE '\n'
@@ -31,6 +42,74 @@ void _ch_readline_setup(void)
 }
 
 
+void _step_backward(char **buffer, size_t *bufindex, size_t *buflen)
+{
+  if (*bufindex > 0) {
+    *bufindex -= 1;
+  }
+}
+
+void _step_forward(char **buffer, size_t *bufindex, size_t *buflen)
+{
+  if (*bufindex < *buflen) {
+    *bufindex += 1;
+  }
+}
+
+void _step_word_forward(char **buffer, size_t *bufindex, size_t *buflen)
+{
+    char *str = *buffer;
+    if (isspace(str[*bufindex])) {
+        while(isspace(str[*bufindex]) && (*bufindex < *buflen)) {
+            *bufindex += 1;
+        }
+
+    }
+    while(!isspace(str[*bufindex]) && (*bufindex < *buflen)) {
+        *bufindex += 1;
+    } 
+}
+
+void _step_word_backward(char **buffer, size_t *bufindex, size_t *buflen)
+{
+    char *str = *buffer;
+    if (isspace(str[*bufindex])) {
+        while(isspace(str[*bufindex]) && (*bufindex > 0)) {
+            *bufindex -= 1;
+        }
+
+    }
+    while(!isspace(str[*bufindex]) && (*bufindex > 0)) {
+        *bufindex -= 1;
+    } 
+}
+
+void _handle_esc(char **buffer, size_t *bufindex, size_t *buflen)
+{
+  char c = getc(stdin);
+  if (c == '[') {
+    c = getc(stdin);
+  }
+  switch(c) {
+  case ESC_UP:
+    break;
+  case ESC_DOWN:
+    break;
+  case ESC_LEFT:
+    _step_backward(buffer, bufindex, buflen);
+    break;
+  case ESC_RIGHT:
+    _step_forward(buffer, bufindex, buflen);
+    break;
+  case 'f':
+    _step_word_forward(buffer, bufindex, buflen);
+    break;
+  case 'b':
+    _step_word_backward(buffer, bufindex, buflen);
+  }
+}
+
+
 char* ch_readline(const char *prompt, size_t promptlen)
 {
   _ch_readline_setup();
@@ -38,6 +117,7 @@ char* ch_readline(const char *prompt, size_t promptlen)
   char* buffer = malloc(sizeof(char) * DEFAULT_BUF_SIZE);
   buffer[DEFAULT_BUF_SIZE - 1] = '\0';
   size_t bufindex = 0;
+  size_t buflen = 0;
   size_t bufsize = DEFAULT_BUF_SIZE;
   char c;
   if (!prompt) {
@@ -51,6 +131,16 @@ char* ch_readline(const char *prompt, size_t promptlen)
     switch (c) {
     case KEY_NEWLINE:
       break;
+    case ESC_NUL:
+    case ESC_VAL:
+      _handle_esc(&buffer, &bufindex, &buflen);
+      break;
+    case CTRL_B:
+      _step_backward(&buffer, &bufindex, &buflen);
+      break;
+    case CTRL_F:
+      _step_forward(&buffer, &bufindex, &buflen);
+      break;
     case KEY_DELETE: //Do same as backspace for now
     case KEY_BCKSPC:
       if (bufindex == 0) {
@@ -60,13 +150,23 @@ char* ch_readline(const char *prompt, size_t promptlen)
 	buffer[i-1] = buffer[i];
       }
       bufindex -= 1;
+      buflen -= 1;
       break;
     default:
+      for (i=buflen+1; i>bufindex; --i) {
+	buffer[i] = buffer[i-1];
+      }
       buffer[bufindex] = c;
       bufindex += 1;
+      buflen += 1;
+
+      if (buflen >= bufsize) {
+	bufsize *= 2;
+	buffer = realloc(buffer, bufsize);
+      }
     }
 
-    buffer[bufindex] = '\0';
+    buffer[buflen] = '\0';
     clearline();
     printf("%s%s", prompt, buffer);
     cursorpos(bufindex+promptlen);
